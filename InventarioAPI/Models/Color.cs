b4944fb79765db35853cb699d227f403e982e36d
David <dvd.campos17@gmail.com>
2021-04-22 14:44:54 -0600

    Se crean los principales modelos

---
+++ InventarioAPI/Models/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventarioAPI.Models
+{
+    public class Colores
+    {
+        public int idColor { get; set; }
+        public string color { get; set; }
+    }
+}
---
+++ InventarioAPI/Models/Marca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventarioAPI.Models
+{
+    public class Marca
+    {
+        public int idMarca { get; set; }
+        public string marca { get; set; }
+        public DateTime fechaCreacion { get; set; }
+        public DateTime fechaModificacion { get; set; }
+        public int idUsuario { get; set; }
+    }
+}
