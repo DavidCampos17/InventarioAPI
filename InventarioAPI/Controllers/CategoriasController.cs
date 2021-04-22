6d01dcc67e14c13fe75b6e651f871f8628a572aa
David <dvd.campos17@gmail.com>
2021-04-22 15:04:05 -0600

    Se crea el controller de Categorias y se listan las categorias

---
+++ InventarioAPI/Controllers/CategoriasController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventarioAPI.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace InventarioAPI.Controllers
+{
+    [ApiController]
+    [Route("api/Categorias")]
+    public class CategoriasController : Controller
+    {
+
+        private readonly IConfiguration _IConfiguration;
+        public CategoriasController(IConfiguration configuration)
+        {
+            _IConfiguration = configuration;
+        }
+
+
+        [HttpGet]
+        [Route("/getCategorias")]
+        public IActionResult getCategorias()
+        {
+            Conexion objConexion = new Conexion(_IConfiguration);
+            var cn = objConexion.getConexion();
+            List<Categoria> listaCategorias = new List<Categoria>();
+            try
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("spListarCategorias", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        listaCategorias.Add(new Categoria
+                        {
+                            categoria = dr["categoria"].ToString(),
+                            idCategoria = int.Parse(dr["idCategoria"].ToString())
+                        });
+                    }
+                }
+                return Ok(listaCategorias);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+                cn.Close();
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+    }
+}
