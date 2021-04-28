2015f3d31f512a6948dec402da603054de2ec97f
David <dvd.campos17@gmail.com>
2021-04-28 15:41:37 -0600

    Se completa el CRUD de Categorias y se agrega el Script de BD.

--- InventarioAPI/Controllers/CategoriasController.cs
+++ InventarioAPI/Controllers/CategoriasController.cs
@@ -88,5 +88,119 @@ namespace InventarioAPI.Controllers
                 cn.Close();
             }
         }
+
+        [HttpGet]
+        [Route("api/Categorias/getCategoria/{idCategoria}")]
+        public IActionResult getCategoria(int idCategoria)
+        {
+            Conexion objConexion = new Conexion(_IConfiguration);
+            var cn = objConexion.getConexion();
+            Categoria objCategoria = null;
+            try
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("spGetCategoria", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@idCategoria", SqlDbType.Int).Value = idCategoria;
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+
+                        objCategoria = new Categoria
+                        {
+                            categoria = dr["categoria"].ToString(),
+                            idCategoria = int.Parse(dr["idCategoria"].ToString())
+                        };
+                    }
+                }
+                return Ok(objCategoria);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+                cn.Close();
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        [HttpPut]
+        [Route("/api/Categorias/ActualizarCategoria")]
+        public IActionResult actualizarCategoria(Categoria objCategoria)
+        {
+            Conexion objConexion = new Conexion(_IConfiguration);
+            var cn = objConexion.getConexion();
+            string msj = "";
+            try
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("spActualizarCategoria", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@idCategoria", SqlDbType.NVarChar).Value = objCategoria.idCategoria;
+                    cmd.Parameters.Add("@categoria", SqlDbType.NVarChar).Value = objCategoria.categoria;
+                    int i = cmd.ExecuteNonQuery();
+                    if (i == 1)
+                    {
+                        return Ok(new { msj = "Se actualizó la categoría " });
+                    }
+                    else
+                    {
+                        return Ok(new { msj = "La categoría " + objCategoria.categoria + " no se actualizó" });
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                cn.Close();
+                return BadRequest();
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        [HttpDelete]
+        [Route("/api/Categorias/delCategoria/{idCategoria}")]
+        public IActionResult delCategoria(int idCategoria)
+        {
+            Conexion objConexion = new Conexion(_IConfiguration);
+            var cn = objConexion.getConexion();
+            string msj = "";
+            try
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("spDelCategoria", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@idCategoria", SqlDbType.Int).Value = idCategoria;
+                    int i = cmd.ExecuteNonQuery();
+                    if (i == 1)
+                    {
+                        return Ok(new { msj = "Se a eliminado " + i + " categoria." });
+                    }
+                    else
+                    {
+                        return Ok(new { msj = "La categoría no se eliminó" });
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+                cn.Close();
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
     }
 }
